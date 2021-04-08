b50401ce48d42accc91a1f6ea32887df8a413b7e
F1nnM <github@mfinn.de>
2021-04-08 21:08:53 +0200

    Added footer svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,43 +6,59 @@ import Twitter from '@material-ui/icons/Twitter'
 import Instagram from '@material-ui/icons/Instagram'
 import Mail from '@material-ui/icons/Mail'
 import LinkedIn from '@material-ui/icons/LinkedIn'
-import CopyrightIcon from '@material-ui/icons/Copyright';
+import React from "react";
 
 function Footer() {
   return (
-    <Container fluid className={styles.footerContainer+" py-3"}>
-      <Row className="py-2 text-uppercase">
-        <Col lg={1} xl={2}/>
-        <Col>Impressum</Col>
-        <Col>Privacy Policy</Col>
-        <Col>
-          <Container>
-            <Row>
-              <Col>
-                <Twitter/>
-              </Col>
-              <Col>
-                <Instagram/>
-              </Col>
-              <Col>
-                <LinkedIn/>
-              </Col>
-              <Col>
-                <Mail/>
-              </Col>
-              
-            </Row>
-          </Container>
-        </Col>
-        <Col lg={1} xl={2}/>
-      </Row>
+    <React.Fragment>
+      <Container fluid className={styles.footerLeadIn}>
+        <Row>
+          <Col>
+            <svg viewBox="0 0 200 16">
+              <g>
+                <path
+                  id="rect833"
+                  style={{ "fill": "#000000" }}
+                  d="M 0,4.7433767 C 71.509152,33.446133 112.41464,-13.201926 199.93429,3.8725987 L 199.99999,16 H 0.0421015 Z" />
+              </g>
+            </svg>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className={styles.footerContainer + " py-3"}>
+        <Row className="py-2 text-uppercase">
+          <Col lg={1} xl={2} />
+          <Col>Impressum</Col>
+          <Col>Privacy Policy</Col>
+          <Col>
+            <Container>
+              <Row>
+                <Col>
+                  <Twitter />
+                </Col>
+                <Col>
+                  <Instagram />
+                </Col>
+                <Col>
+                  <LinkedIn />
+                </Col>
+                <Col>
+                  <Mail />
+                </Col>
 
-      <Row className="py-2">
-        <Col className="text-center">
-          <span>&copy; ActionPaint 2021</span>
-        </Col>
-      </Row>
-    </Container>
+              </Row>
+            </Container>
+          </Col>
+          <Col lg={1} xl={2} />
+        </Row>
+
+        <Row className="py-2">
+          <Col className="text-center">
+            <span>&copy; ActionPaint 2021</span>
+          </Col>
+        </Row>
+      </Container>
+    </React.Fragment>
   );
 }
 
