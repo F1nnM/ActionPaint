3496c69c4b3382ef459904714e4f23160d1c3e5e
F1nnM <github@mfinn.de>
2021-04-09 21:42:10 +0200

    Reworked footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,11 @@ import styles from './Footer.module.css'
 
 import Twitter from '@material-ui/icons/Twitter'
 import Instagram from '@material-ui/icons/Instagram'
-import Mail from '@material-ui/icons/Mail'
+import ContactMail from '@material-ui/icons/ContactMail'
 import LinkedIn from '@material-ui/icons/LinkedIn'
+import Phone from '@material-ui/icons/Phone'
+import Business from '@material-ui/icons/Business';
+import LocationOn from '@material-ui/icons/LocationOn';
 import React from "react";
 
 function Footer() {
@@ -26,26 +29,33 @@ function Footer() {
         </Row>
       </Container>
       <Container fluid className={styles.footerContainer + " py-3"}>
-        <Row className="py-2 text-uppercase">
+        <Row className="py-2">
           <Col lg={1} xl={2} />
-          <Col>Impressum</Col>
-          <Col>Privacy Policy</Col>
+          <Col>
+            <span className="text-uppercase">Impressum - Legal Notice:</span>
+            <address className="mt-3">
+              <p><Business className='mr-3' />ActionPaint Agency GmbH</p>
+              <p><LocationOn className='mr-3' />Max-Muster Straße 42, 12345 Berlin</p>
+              <p><ContactMail className='mr-3' /><a href="mailto:jim@rock.com" className={styles.colorReset}>contact@actionpaint.de</a></p>
+              <p><Phone className='mr-3' /><a href="tel:+13115552368" className={styles.colorReset}>(+49) 123 456 789</a></p>
+            </address>
+          </Col>
+          <Col>
+            <span className="text-uppercase">Privacy Policy</span>
+            <p className="mt-3">We don't collect any data from you, unless you reach out to us. For details click <a href="privacyPolicy.txt" className={styles.colorReset+" "+styles.underline}>here</a>.</p>
+          </Col>
           <Col>
             <Container>
               <Row>
                 <Col>
-                  <Twitter />
+                  <a href="https://twitter.com" className={styles.colorReset}><Twitter /></a>
                 </Col>
                 <Col>
-                  <Instagram />
+                  <a href="https://instagram.com" className={styles.colorReset}><Instagram /></a>
                 </Col>
                 <Col>
-                  <LinkedIn />
+                  <a href="https://linkedin.com" className={styles.colorReset}><LinkedIn /></a>
                 </Col>
-                <Col>
-                  <Mail />
-                </Col>
-
               </Row>
             </Container>
           </Col>
