8cc38606a7402fe769f918e4e5f757de090f8f6d
Bernd Mayinger <berndmayinger@gmail.com>
2021-04-10 19:24:55 +0200

    improved social media buttons a bit, saved ceo image locally

--- src/data/team.js
+++ src/data/team.js
@@ -7,16 +7,14 @@ var team = {
       age: 20,
       info: "Hier könnte Ihre Werbung stehen",
       githubName: "Fabi281",
-      imageUrl:
-        "https://pixabay.com/get/gd2535c2947039ab086c872e56438a566109ed25849b0e21ccbbd784777ca3b3d1ef4c3bde4897954f8f0ec232394e1fe029332aa7a3a0dbb98621d80e333dd61_1920.jpg",
+      imageUrl: "lorem_ceo",
     },
     {
       id: 2,
       name: "Finn",
       age: 20,
       info: "Hier könnte Ihre Werbung stehen",
-      imageUrl:
-        "https://pixabay.com/get/gd2535c2947039ab086c872e56438a566109ed25849b0e21ccbbd784777ca3b3d1ef4c3bde4897954f8f0ec232394e1fe029332aa7a3a0dbb98621d80e333dd61_1920.jpg",
+      imageUrl: "lorem_ceo",
     },
     {
       id: 3,
