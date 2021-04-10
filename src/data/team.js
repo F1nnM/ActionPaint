1ceb2af8f7112321ab6e2d237cea950dd1c0c013
Bernd Mayinger <berndmayinger@gmail.com>
2021-04-10 18:17:22 +0200

    seperate style module for about us, insta and github icons added without styling

--- src/data/team.js
+++ src/data/team.js
@@ -2,19 +2,28 @@ var team = {
   info: "So...what to write here? Well, we're a cool team.",
   members: [
     {
+      id: 1,
       name: "Fabian",
       age: 20,
       info: "Hier könnte Ihre Werbung stehen",
+      githubName: "Fabi281",
+      imageUrl:
+        "https://pixabay.com/get/gd2535c2947039ab086c872e56438a566109ed25849b0e21ccbbd784777ca3b3d1ef4c3bde4897954f8f0ec232394e1fe029332aa7a3a0dbb98621d80e333dd61_1920.jpg",
     },
     {
+      id: 2,
       name: "Finn",
       age: 20,
       info: "Hier könnte Ihre Werbung stehen",
+      imageUrl:
+        "https://pixabay.com/get/gd2535c2947039ab086c872e56438a566109ed25849b0e21ccbbd784777ca3b3d1ef4c3bde4897954f8f0ec232394e1fe029332aa7a3a0dbb98621d80e333dd61_1920.jpg",
     },
     {
+      id: 3,
       name: "Bernd",
       age: 20,
       info: "Hier könnte Ihre Werbung stehen",
+      instaName: "da.bernd",
     },
   ],
 };
