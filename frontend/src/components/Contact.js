dda7f45838062ab6f3d208f66ac5f2a765b6d026
Bernd Mayinger <berndmayinger@gmail.com>
2021-04-15 17:02:43 +0200

    removed contact warning, added logic to contact form

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,51 +1,66 @@
-// unused import -> warnung -> wird nicht compiled.
-//import styles from "./Contact.module.css";
+import styles from "./Contact.module.css";
+import { useState } from "react";
 
-function Contact() {
-  var state = {};
+function Contact(props) {
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const send = (e) => {
+    e.preventDefault();
+
+    alert("Message from " + name + " <" + mail + "> has been sent!");
+  };
 
   return (
-      <form id="contact-form" onSubmit={handleSubmit.bind(this)} method="POST">
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            value={state.name}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-            value={state.mail}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea
-            className="form-control"
-            rows="5"
-            id="message"
-            value={state.message}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+    <form id="contact-form" onSubmit={send} method="POST">
+      <div className="form-group">
+        <label htmlFor="name" className={styles.lorem}>
+          Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          required
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Mail address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          required
+          aria-describedby="emailHelp"
+          value={mail}
+          onChange={(e) => {
+            setMail(e.target.value);
+          }}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea
+          className="form-control"
+          rows="5"
+          required
+          id="message"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
   );
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  console.log(event);
-
-  alert(event);
 }
 
 export default Contact;
