ce209130bf453f30335d60a10e30310247cd515f
F1nnM <github@mfinn.de>
2021-04-16 11:53:55 +0200

    Fix

--- backend/index.js
+++ backend/index.js
@@ -99,5 +99,5 @@ app.post("/public_api/:action", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`ActionPaint backend listening at http://localhost:${port}`);
 });
