259ae2c5c00b8943af1f0713c3928029c4aa3b56
Fabi281 <Fabian.Meindl@freenet.de>
2021-04-17 12:42:59 +0200

    Added Smooth Scrolling on Navbar jumps

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,25 +4,46 @@ import PersonIcon from '@material-ui/icons/Person';
 
 function NavbarFunc ({tmpinView, data}){
 
+  var Scroll = require('react-scroll');
+  var Events = Scroll.Events;
+  var Link = Scroll.Link;
+  var Scrollspeed = 500;
+
+  Events.scrollEvent.register('begin', function(to, element) {
+    console.log('begin', to, element);
+  });
+  Events.scrollEvent.register('end', function(to, element) {
+    console.log('end', to, element);
+  });
+  Scroll.scrollSpy.update();
+
   const artists = data.artists
 
   return(
     <Navbar expand={tmpinView ? !tmpinView : "sm"} fixed="top" className={tmpinView ? "" : styles.navbar}>
-    <Navbar.Brand href={"#TitleScreen"} className={tmpinView ? styles.navbaralt : styles.navbar}>ActionPaint</Navbar.Brand>
+    <Link activeClass="active" className={tmpinView ? styles.navbaralt : styles.navbar} to="TitleScreen" spy={true} smooth={true} duration={Scrollspeed}>
+      <Navbar.Brand className={tmpinView ? styles.navbaralt : styles.navbar}>
+        ActionPaint
+      </Navbar.Brand>
+    </Link>
     <Navbar.Toggle className={styles.hamburger}/>
     <Navbar.Collapse className={!tmpinView ? styles.navbar : styles.collapseabletext} >
       <Nav>
         <NavDropdown title="Top Artists">
         {artists.map((ar, index) => (
-          <NavDropdown.Item key={"NavDropdown"+index} className={styles.dropitem} href={"#"+ar.lastName+index}>
-            <NavDropdown.ItemText className={styles.dropitem}><PersonIcon />{ar.firstName} {ar.lastName}</NavDropdown.ItemText>
-          </NavDropdown.Item>
+          <Link key={"Link"+index} activeClass="active" to={ar.lastName+index} spy={true} smooth={true} duration={Scrollspeed}>
+            <NavDropdown.Item>
+              <NavDropdown.ItemText className={styles.dropitem}>
+                <PersonIcon />{ar.firstName} {ar.lastName}
+              </NavDropdown.ItemText>
+            </NavDropdown.Item>
+          </Link>
           ))}
         </NavDropdown>
-        <Nav.Link href={"#About Us"}>About Us</Nav.Link>
-        <Nav.Link href={"#What We Do"}>What We Do</Nav.Link>
-        <Nav.Link href={"#FAQ"}>FAQ</Nav.Link>
-        <Nav.Link href={"#Contact Us"}>Contact</Nav.Link>
+        <Link activeClass="active" to="About Us" spy={true} smooth={true} duration={Scrollspeed}><Nav.Link>About Us</Nav.Link></Link>
+        <Link activeClass="active" to="What We Do" spy={true} smooth={true} duration={Scrollspeed}><Nav.Link>What We Do</Nav.Link></Link>
+        <Link activeClass="active" to="FAQ" spy={true} smooth={true} duration={Scrollspeed}><Nav.Link>FAQ</Nav.Link></Link>
+        <Link activeClass="active" to="Contact Us" spy={true} smooth={true} duration={Scrollspeed}><Nav.Link>Contact</Nav.Link></Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
