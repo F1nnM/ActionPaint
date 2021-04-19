2f16709eda091473858725aaa4ba4dcdf0b6da1d
Bernd Mayinger <berndmayinger@gmail.com>
2021-04-19 16:01:57 +0200

    fixed some design bugs in TopFlayors

--- frontend/src/components/TopFlavors.js
+++ frontend/src/components/TopFlavors.js
@@ -6,9 +6,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import ResponsiveEmbed from "react-bootstrap/ResponsiveEmbed";
 
-function TopFlavors({data}) {
-
+function TopFlavors({ data }) {
   const artists = data.artists;
 
   /* const [index, setIndex] = useState(0);
@@ -25,20 +25,30 @@ function TopFlavors({data}) {
   return (
     <div onScroll={handleScroll}>
       {artists.map((artist, index) => (
-        <Container key={artist.lastName+index} id={artist.lastName+index} className="mt-4">
-          <Row className={styles.artistHeight}>
+        <Container
+          key={artist.lastName + index}
+          id={artist.lastName + index}
+          className="mt-4"
+          style={{ height: "40vh" }}
+        >
+          <Row>
             <Col
               sm={12}
               md={{ span: 7, order: (index + 1) % 2 === 0 ? "last" : "first" }}
             >
-              <Image
-                className={"d-block w-100 "}
-                src={
-                  process.env.REACT_APP_BACKEND +
-                  "images/artist/" + artist.images[0]
-                }
-                rounded
-              />
+              <div style={{ height: "100%" }}>
+                <ResponsiveEmbed aspectRatio="16by9">
+                  <Image
+                    className={"d-block w-100 " + styles.workImg}
+                    src={
+                      process.env.REACT_APP_BACKEND +
+                      "images/artist/" +
+                      artist.images[0]
+                    }
+                    rounded
+                  />
+                </ResponsiveEmbed>
+              </div>
             </Col>
             <Col sm={12} md={5}>
               <Card>
