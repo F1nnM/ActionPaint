c278ef9e79282c3bfa1fdad4aef10a7cebab2ee8
Bernd Mayinger <berndmayinger@gmail.com>
2021-04-20 10:54:47 +0200

    public api post working

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
 //load .env file
-require('dotenv').config();
-
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
 const port = 4000;
 
-const content = require('./content')
+const content = require("./content");
 const fs = require("fs");
 
 //===========================================
 // CORS stuff
 //===========================================
 if (process.env.ALLOW_CORS) {
-  const cors = require('cors')
-  app.use(cors({
-    origin: "*"
-  }))
+  const cors = require("cors");
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
 }
-
 
 //===========================================
 // content serving
@@ -32,22 +32,21 @@ app.use("/images", express.static("images"));
 
 content.init();
 app.get("/content", (req, res) => {
-  res.json(content.content)
+  res.json(content.content);
 });
-
-
 
 //===========================================
 // Auth for admin section
 //===========================================
 
-const basicAuth = require("express-basic-auth")
-app.use("/api", basicAuth({
-  users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASS },
-  challenge: true,
-}))
-
-
+const basicAuth = require("express-basic-auth");
+app.use(
+  "/api",
+  basicAuth({
+    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASS },
+    challenge: true,
+  })
+);
 
 //===========================================
 // file upload handling
@@ -55,40 +54,39 @@ app.use("/api", basicAuth({
 
 // generate which folter to save a image to
 function destination(req, file, callback) {
-  callback(null, `./images/${req.body.imagetype}/`)
+  callback(null, `./images/${req.body.imagetype}/`);
 }
 // generate a filename for the image
 function filename(req, file, callback) {
-  callback(null, file.originalname + "_" + Date.now())
+  callback(null, file.originalname + "_" + Date.now());
 }
 
 // validate uploaded files, only allow correct image types
 const validTypes = ["artist", "team"];
 function fileFilter(req, file, callback) {
-  callback(null, validTypes.includes(req.body.imagetype))
+  callback(null, validTypes.includes(req.body.imagetype));
 }
 
 // multer handles the multipart/form requests
 var multer = require("multer");
 var storage = multer.diskStorage({
   destination,
-  filename
-})
+  filename,
+});
 
 var upload = multer({
   storage,
-  fileFilter
-})
+  fileFilter,
+});
 
 // define endpoint for uploading images, multer middleware, 200 return code
-app.post("/api/upload_image", upload.array("images"), (req, res) => res.status(200).end());
-
-
+app.post("/api/upload_image", upload.array("images"), (req, res) =>
+  res.status(200).end()
+);
 
 //===========================================
 // other api handling
 //===========================================
-
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: false }));
@@ -107,23 +105,25 @@ app.post("/api/update/:file", (req, res) => {
     return;
   }
 
-  fs.writeFile("./content/" + file + ".json", req.body.content, "utf-8", (err) => {
-    if (err)
-      res.status(500).send(err)
-    else {
-      res.status(200).send("File updated");
-      content.update();
+  fs.writeFile(
+    "./content/" + file + ".json",
+    req.body.content,
+    "utf-8",
+    (err) => {
+      if (err) res.status(500).send(err);
+      else {
+        res.status(200).send("File updated");
+        content.update();
+      }
     }
-  })
+  );
 });
 
 app.post("/public_api/:action", (req, res) => {
   // All the api handling goes here
   let action = public_actions[req.params.action];
-  if (action)
-    action(req, res)
-  else
-    res.status(400).send("No valid action")
+  if (action) action(req, res);
+  else res.status(400).send("No valid action");
 });
 
 app.listen(port, "0.0.0.0", () => {
--- frontend/src/components/PlaygroundComponent.js
+++ frontend/src/components/PlaygroundComponent.js
@@ -1,6 +1,24 @@
 // import styles from "./ExampleSection.module.css";
 
 function PlaygroundComponent() {
+  const url = process.env.REACT_APP_BACKEND + "public_api/ping";
+  const options = {
+    method: "POST",
+    body: JSON.stringify({
+      nsame: "John",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  fetch(url, options)
+    .then((res) => res.json())
+    .catch((err) => {
+      alert(err);
+    })
+    .then((data) => {
+      alert(data.value);
+    });
   return (
     <div>
       <p>you can use this component for testing purposes</p>
