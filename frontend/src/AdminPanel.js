91b5ba6aefca94bdec63ef95d587a5ddc547c939
Bernd Mayinger <berndmayinger@gmail.com>
2021-05-02 21:16:00 +0200

    made AboutUs admin: only info working

--- frontend/src/AdminPanel.js
+++ frontend/src/AdminPanel.js
@@ -1,14 +1,14 @@
 import NavFrame from "./admin_components/NavFrame";
 import FAQ from "./admin_components/FAQ";
 import WhatWeDo from "./admin_components/WhatWeDo";
+import AboutUs from "./admin_components/AboutUs";
 import Button from "react-bootstrap/Button";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
-import styles from "./AdminPanel.module.css"
+import styles from "./AdminPanel.module.css";
 
 function AdminPanel({ switchToWeb }) {
-
   const [credentials, setCredentials] = useState({});
   const [usernameInput, setUsernameInput] = useState(null);
   const [passwordInput, setPasswordInput] = useState(null);
@@ -16,31 +16,34 @@ function AdminPanel({ switchToWeb }) {
 
   useEffect(() => {
     fetch(process.env.REACT_APP_BACKEND + "content")
-      .then(resp => resp.json())
-      .then(data => setData(data))
-    }, []);
+      .then((resp) => resp.json())
+      .then((data) => setData(data));
+  }, []);
 
   function tryLogin() {
     let url = process.env.REACT_APP_BACKEND + "admin";
 
     let headers = new Headers();
 
-    headers.append('Authorization', 'Basic ' + btoa(usernameInput + ":" + passwordInput));
+    headers.append(
+      "Authorization",
+      "Basic " + btoa(usernameInput + ":" + passwordInput)
+    );
 
     fetch(url, {
       method: "GET",
-      headers
+      headers,
     })
       .then((res) => {
         if (res.ok) {
-          setCredentials({ username: usernameInput, password: passwordInput })
+          setCredentials({ username: usernameInput, password: passwordInput });
         } else {
           throw new Error("Not authorized");
         }
       })
       .catch((err) => {
-        alert(err)
-      })
+        alert(err);
+      });
   }
 
   if (!credentials.username || !credentials.password)
@@ -48,14 +51,25 @@ function AdminPanel({ switchToWeb }) {
       <Container>
         <Row>
           <Col className={styles.height100 + " d-flex justify-content-center"}>
-            <Form className="d-flex flex-column justify-content-center" action={null}>
+            <Form
+              className="d-flex flex-column justify-content-center"
+              action={null}
+            >
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="user" placeholder="Username" onInput={e => setUsernameInput(e.target.value)} />
+                <Form.Control
+                  type="user"
+                  placeholder="Username"
+                  onInput={(e) => setUsernameInput(e.target.value)}
+                />
               </Form.Group>
               <Form.Group controlId="ControlTextarea1">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" onInput={e => setPasswordInput(e.target.value)} />
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  onInput={(e) => setPasswordInput(e.target.value)}
+                />
               </Form.Group>
               <Button variant="primary" onClick={tryLogin}>
                 Log In
@@ -69,11 +83,15 @@ function AdminPanel({ switchToWeb }) {
   const tabs = [
     {
       label: "FAQ",
-      component: <FAQ data={data} creds={credentials}/>,
+      component: <FAQ data={data} creds={credentials} />,
     },
     {
       label: "What We Do",
-      component: <WhatWeDo data={data} creds={credentials}/>,
+      component: <WhatWeDo data={data} creds={credentials} />,
+    },
+    {
+      label: "About Us",
+      component: <AboutUs data={data} creds={credentials} />,
     },
     {
       label: "Test",
@@ -81,14 +99,10 @@ function AdminPanel({ switchToWeb }) {
     },
     {
       label: "Go back",
-      component: (
-          <Button onClick={switchToWeb}>Logout</Button>
-      ),
+      component: <Button onClick={switchToWeb}>Logout</Button>,
     },
   ];
-  return (
-      <NavFrame tabs={tabs}></NavFrame>
-  );
+  return <NavFrame tabs={tabs}></NavFrame>;
 }
 
 export default AdminPanel;
