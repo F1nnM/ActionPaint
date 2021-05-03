c21d1472f8ddb6f9d4c9f826cf7ff904ec8f44a4
Bernd Mayinger <berndmayinger@gmail.com>
2021-05-03 15:15:12 +0200

    about us dynamic prop editing of existing people

--- frontend/src/admin_components/AboutUs.js
+++ frontend/src/admin_components/AboutUs.js
@@ -1,9 +1,7 @@
 import styles from "./WhatWeDo.module.scss";
 import { Table, Button, Col, Container, Form, Row } from "react-bootstrap";
+import { GitHub, Twitter, Delete } from "@material-ui/icons";
 import { useState } from "react";
-import DeleteIcon from "@material-ui/icons/Delete";
-import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
 function WhatWeDo({ data, creds }) {
   const [aboutUs, setAboutUs] = useState(data.about);
@@ -17,8 +15,20 @@ function WhatWeDo({ data, creds }) {
   );
   headers.append("Content-Type", "application/json");
 
-  function handleUpdateDescription(value) {
+  function handleUpdateInfo(key, value) {
     aboutUs.info = value;
+  }
+
+  function handleUpdateProp(key, value, idx) {
+    aboutUs.members[idx][key] = value;
+  }
+
+  function handleUpdateInfo(value, idx) {
+    aboutUs.members[idx].info = value;
+  }
+
+  function handleDelete(idx) {
+    aboutUs.members.unshift(idx);
   }
 
   function handleUpdateSubmit() {
@@ -53,16 +63,79 @@ function WhatWeDo({ data, creds }) {
                   as="textarea"
                   placeholder="Description"
                   defaultValue={aboutUs.info}
-                  onInput={(e) => handleUpdateDescription(e.target.value)}
+                  onInput={(e) => handleUpdateInfo(e.target.value)}
                 />
               </Form.Group>
-              <Button variant="primary" onClick={() => handleUpdateSubmit()}>
-                Update
-              </Button>
             </Form>
           </Col>
         </Row>
       </Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Info</th>
+            <th>
+              <GitHub />
+            </th>
+            <th>
+              <Twitter />
+            </th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {aboutUs.members.map((entry, idx) => (
+            <tr>
+              {/* <td width="200">
+                <Button variant="danger" onClick={() => handleDelete(idx)}>
+                  <DeleteIcon />
+                </Button>
+                <Button
+                  className="float-right"
+                  variant="primary"
+                  onClick={() => handleMoveUp(idx)}
+                >
+                  <ArrowUpwardIcon />
+                </Button>
+                <p></p>
+                <Button
+                  className="float-right"
+                  variant="primary"
+                  onClick={() => handleMoveDown(idx)}
+                >
+                  <ArrowDownwardIcon />
+                </Button>
+              </td> */}
+              {["name", "age", "info", "githubName", "twitterName"].map(
+                (prop, idx) => (
+                  <td>
+                    <Form.Control
+                      defaultValue={entry[prop]}
+                      onInput={(e) =>
+                        handleUpdateProp(prop, e.target.value, idx)
+                      }
+                    />
+                  </td>
+                )
+              )}
+
+              <td>
+                <Button variant="danger" onClick={() => handleDelete(idx)}>
+                  <Delete />
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <hr />
+      <div className={styles.saveChanges}>
+        <Button variant="success" onClick={() => handleUpdateSubmit()}>
+          Save Changes
+        </Button>
+      </div>
     </>
   );
 }
