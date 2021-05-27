3d609bff2443520f0574274e9874e4d0cb1d795d
F1nnM <github@mfinn.de>
2021-05-27 20:33:54 +0200

    Deleted obsolete example section

--- frontend/src/components/ExampleSection.js
+++
@@ -1,7 +0,0 @@
-import styles from "./ExampleSection.module.scss";
-
-function ExampleSection() {
-  return <span className={styles.exampleClass}>This is some content</span>;
-}
-
-export default ExampleSection;
